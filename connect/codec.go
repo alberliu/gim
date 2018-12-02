d567b8749b4442399e30a59ec25f4c7d036e206c
alberliu <alber_liu@qq.com>
2018-12-02 10:16:58 +0800

    减少读写缓存区内存分配

--- connect/codec.go
+++ connect/codec.go
@@ -8,11 +8,11 @@ import (
 )
 
 const (
-	TypeLen       = 2         // 消息类型字节数组长度
-	LenLen        = 2         // 消息长度字节数组长度
-	HeadLen       = 4         // 消息头部字节数组长度（消息类型字节数组长度+消息长度字节数组长度）
-	ContentMaxLen = 65536     // 消息体最大长度
-	BufLen        = 65536 + 4 // 缓冲buffer字节数组长度
+	TypeLen       = 2                 // 消息类型字节数组长度
+	LenLen        = 2                 // 消息长度字节数组长度
+	HeadLen       = 4                 // 消息头部字节数组长度（消息类型字节数组长度+消息长度字节数组长度）
+	ContentMaxLen = 4092              // 消息体最大长度
+	BufLen        = ContentMaxLen + 4 // 缓冲buffer字节数组长度
 
 )
 
