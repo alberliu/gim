d8d1f6d4feadad96e846d927b15997cb9fc25ce7
liupenghui <liupenghui@mizhua.com>
2019-12-17 09:50:31 +0800

    修改错误处理

--- public/imerror/error.go
+++ public/imerror/error.go
@@ -3,6 +3,7 @@ package imerror
 import (
 	"fmt"
 	"runtime"
+	"strings"
 )
 
 const name = "gim"
@@ -20,29 +21,25 @@ func (e *UnknownError) Error() string {
 func WrapError(err error) error {
 	e := &UnknownError{
 		Err:   err,
-		Stack: Stack(),
+		Stack: stack(),
 	}
 	return e
 }
 
 // Stack 获取堆栈信息
-func Stack() []string {
+func stack() []string {
 	var pc = make([]uintptr, 20)
 	n := runtime.Callers(3, pc)
 
 	stack := make([]string, 0, n)
-	/*for i := 0; i < n; i++ {
-		//f := runtime.FuncForPC(pc[i])
-		//file, line := f.FileLine(pc[i])
-		//n := strings.Index(file, name)
-		//if n != -1 {
-		//s := fmt.Sprintf("%s:%d %s", file, line, f.Name())
-		//stack = append(stack, s)
-		//}
-
-		stack = append(stack, fmt.Sprintf("%+v", pc[i]))
-	}*/
-
-	fmt.Printf("%+v", pc[:n])
+	for i := 0; i < n; i++ {
+		f := runtime.FuncForPC(pc[i] - 1)
+		file, line := f.FileLine(pc[i] - 1)
+		n := strings.Index(file, name)
+		if n != -1 {
+			s := fmt.Sprintf("%s:%d %s", file, line, f.Name())
+			stack = append(stack, s)
+		}
+	}
 	return stack
 }
--- public/imerror/error_test.go
+++ public/imerror/error_test.go
@@ -9,28 +9,18 @@ import (
 func TestError(t *testing.T) {
 	err := a()
 	e := err.(*UnknownError)
-	fmt.Println(e.Err)
 	for _, v := range e.Stack {
 		fmt.Println(v)
 	}
 }
 
 func a() error {
-	fmt.Println("11111111111111111")
 	return b()
 }
 
 func b() error {
-	fmt.Println("11111111111111111")
 	return c()
 }
 func c() error {
-	fmt.Println("11111111111111111")
-	fmt.Println("11111111111111111")
-	fmt.Println("11111111111111111")
-	fmt.Println("11111111111111111")
-	fmt.Println("11111111111111111")
-	fmt.Println("11111111111111111")
-	fmt.Println("11111111111111111")
 	return WrapError(errors.New("kkk"))
 }
