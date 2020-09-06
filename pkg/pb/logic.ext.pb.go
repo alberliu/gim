9dc4fdcca4bef666d379c0796351b7f8f6692b5d
alber <alber_liu@qq.com>
2020-09-06 11:57:33 +0800

    添加用户信息设置接口

--- pkg/pb/logic.ext.pb.go
+++ pkg/pb/logic.ext.pb.go
@@ -1622,11 +1622,11 @@ var fileDescriptor_f5bf3fa139aedf16 = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConn
+var _ grpc.ClientConnInterface
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion4
+const _ = grpc.SupportPackageIsVersion6
 
 // LogicExtClient is the client API for LogicExt service.
 //
@@ -1661,10 +1661,10 @@ type LogicExtClient interface {
 }
 
 type logicExtClient struct {
-	cc *grpc.ClientConn
+	cc grpc.ClientConnInterface
 }
 
-func NewLogicExtClient(cc *grpc.ClientConn) LogicExtClient {
+func NewLogicExtClient(cc grpc.ClientConnInterface) LogicExtClient {
 	return &logicExtClient{cc}
 }
 
