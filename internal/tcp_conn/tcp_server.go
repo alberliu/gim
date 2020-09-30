07391337f4069718964fc790914a5137d28d698f
alber <liupenghui@mizhua.com>
2020-09-30 17:18:59 +0800

    commit

--- internal/tcp_conn/tcp_server.go
+++ internal/tcp_conn/tcp_server.go
@@ -14,7 +14,11 @@ var server *gn.Server
 
 func StartTCPServer() {
 	var err error
-	server, err = gn.NewServer(config.TCPConn.TCPListenAddr, &handler{}, gn.NewHeaderLenDecoder(2, 254), gn.WithTimeout(5*time.Minute, 11*time.Minute))
+	server, err = gn.NewServer(config.TCPConn.TCPListenAddr, &handler{},
+		gn.NewHeaderLenDecoder(2, 254),
+		gn.WithTimeout(5*time.Minute, 11*time.Minute),
+		gn.WithAcceptGNum(10),
+		gn.WithIOGNum(100))
 	if err != nil {
 		logger.Sugar.Error(err)
 		panic(err)
