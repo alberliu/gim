938b9b5a13e8653154a2f75cb4c911e1a245f419
alber <alber_liu@qq.com>
2020-12-27 01:01:03 +0800

    commit

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"gim/config"
+	"gim/pkg/logger"
 
 	"github.com/go-redis/redis"
 	"github.com/jinzhu/gorm"
@@ -16,6 +17,7 @@ var (
 
 // InitMysql 初始化MySQL
 func InitMysql(dataSource string) {
+	logger.Logger.Info("init mysql")
 	var err error
 	DB, err = gorm.Open("mysql", dataSource)
 	if err != nil {
@@ -23,10 +25,12 @@ func InitMysql(dataSource string) {
 	}
 	DB.SingularTable(true)
 	DB.LogMode(true)
+	logger.Logger.Info("init mysql ok")
 }
 
 // InitRedis 初始化Redis
 func InitRedis(addr, password string) {
+	logger.Logger.Info("init redis")
 	RedisCli = redis.NewClient(&redis.Options{
 		Addr:     addr,
 		DB:       0,
@@ -37,6 +41,8 @@ func InitRedis(addr, password string) {
 	if err != nil {
 		panic(err)
 	}
+
+	logger.Logger.Info("init redis ok")
 }
 
 // InitByTest 初始化数据库配置，仅用在单元测试
