a0813e6249565639b8cf06bd644a18b6588707a9
alber <alber_liu@qq.com>
2021-06-15 17:58:20 +0800

    修改配置文件

--- config/local_conf.go
+++ config/local_conf.go
@@ -8,10 +8,10 @@ import (
 
 func initLocalConf() {
 	Logic = LogicConf{
-		MySQL:            "root:liu123456@tcp(localhost:3307)/gim?charset=utf8&parseTime=true&loc=Local",
-		NSQIP:            "127.0.0.1:4150",
-		RedisIP:          "127.0.0.1:6379",
-		RedisPassword:    "liu123456",
+		MySQL:            "root:gim123456@tcp(111.229.238.28:3306)/gim?charset=utf8&parseTime=true",
+		NSQIP:            "111.229.238.28:4150",
+		RedisIP:          "111.229.238.28:6379",
+		RedisPassword:    "alber123456",
 		RPCIntListenAddr: ":50000",
 		RPCExtListenAddr: ":50001",
 		ConnectRPCAddrs:  "addrs:///127.0.0.1:50100,127.0.0.1:50200",
@@ -25,19 +25,19 @@ func initLocalConf() {
 		LocalAddr:     "127.0.0.1:50100",
 		LogicRPCAddrs: "addrs:///127.0.0.1:50000",
 		RedisIP:       "111.229.238.28:6379",
-		RedisPassword: "",
+		RedisPassword: "alber123456",
 		SubscribeNum:  100,
 	}
 
 	Business = BusinessConf{
-		MySQL:            "root:liu123456@tcp(localhost:3306)/im?charset=utf8&parseTime=true",
-		NSQIP:            "127.0.0.1:4150",
-		RedisIP:          "127.0.0.1:6379",
+		MySQL:            "root:gim123456@tcp(111.229.238.28:3306)/gim?charset=utf8&parseTime=true",
+		NSQIP:            "111.229.238.28:4150",
+		RedisIP:          "111.229.238.28:6379",
+		RedisPassword:    "alber123456",
 		RPCIntListenAddr: ":50300",
 		RPCExtListenAddr: ":50301",
 		LogicRPCAddrs:    "addrs:///127.0.0.1:50000",
 	}
-
 	logger.Leavel = zap.DebugLevel
 	logger.Target = logger.Console
 }
