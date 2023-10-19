125bbf330cac2c052ccedc21fee3f0eba4f3891b
alber <alber_liu@qq,com>
2023-10-19 18:37:55 +0800

    修复seq并发读取异常问题

--- internal/logic/domain/message/repo/seq_repo.go
+++ internal/logic/domain/message/repo/seq_repo.go
@@ -21,18 +21,18 @@ func (*seqRepo) Incr(objectType int, objectId int64) (int64, error) {
 	defer tx.Rollback()
 
 	var seq int64
-	err := db.DB.Raw("select seq from seq where object_type = ? and object_id = ? for update", objectType, objectId).
+	err := tx.Raw("select seq from seq where object_type = ? and object_id = ? for update", objectType, objectId).
 		Row().Scan(&seq)
 	if err != nil && err != sql.ErrNoRows {
 		return 0, gerrors.WrapError(err)
 	}
 	if err == sql.ErrNoRows {
-		err = db.DB.Exec("insert into seq (object_type,object_id,seq) values (?,?,?)", objectType, objectId, seq+1).Error
+		err = tx.Exec("insert into seq (object_type,object_id,seq) values (?,?,?)", objectType, objectId, seq+1).Error
 		if err != nil {
 			return 0, gerrors.WrapError(err)
 		}
 	} else {
-		err = db.DB.Exec("update seq set seq = seq + 1 where object_type = ? and object_id = ?", objectType, objectId).Error
+		err = tx.Exec("update seq set seq = seq + 1 where object_type = ? and object_id = ?", objectType, objectId).Error
 		if err != nil {
 			return 0, gerrors.WrapError(err)
 		}
