eb08bc7715fd598fa2a8b3e11a1da0d5bfac3de4
alber <alber_liu@qq.com>
2025-12-29 09:45:51 +0800

    refactor: 优化序列号自增方法，使用UPSERT替代FOR UPDATE

--- internal/logic/message/repo/seq.go
+++ internal/logic/message/repo/seq.go
@@ -17,20 +17,20 @@ type seqRepo struct{}
 var SeqRepo = new(seqRepo)
 
 // Incr 自增seq,并且获取自增后的值
-func (*seqRepo) Incr(ctx context.Context, objectType int, objectId uint64) (uint64, error) {
+func (*seqRepo) Incr(ctx context.Context, objectType int, objectID uint64) (uint64, error) {
 	var seq uint64
 	err := db.DB.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
 		// UPSERT: 插入或更新
 		err := tx.Exec(
 			"INSERT INTO seq (object_type, object_id, seq) VALUES (?, ?, 1) "+
 				"ON DUPLICATE KEY UPDATE seq = seq + 1",
-			objectType, objectId).Error
+			objectType, objectID).Error
 		if err != nil {
 			return err
 		}
 		// 在同一事务中查询当前值
 		return tx.Raw("SELECT seq FROM seq WHERE object_type = ? AND object_id = ?",
-			objectType, objectId).Row().Scan(&seq)
+			objectType, objectID).Row().Scan(&seq)
 	})
 	if err != nil {
 		return 0, err
