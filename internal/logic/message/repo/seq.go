45a435038c792cbe2634f53adf90121c3db90528
alber <alber_liu@qq.com>
2025-12-29 09:44:08 +0800

    refactor: 优化序列号自增方法，使用UPSERT替代FOR UPDATE

--- internal/logic/message/repo/seq.go
+++ internal/logic/message/repo/seq.go
@@ -2,15 +2,14 @@ package repo
 
 import (
 	"context"
-	"database/sql"
-	"errors"
+
+	"gorm.io/gorm"
 
 	"gim/pkg/db"
 )
 
 const (
 	SeqObjectTypeUser = 1 // 用户
-	SeqObjectTypeRoom = 2 // 房间
 )
 
 type seqRepo struct{}
@@ -19,27 +18,22 @@ var SeqRepo = new(seqRepo)
 
 // Incr 自增seq,并且获取自增后的值
 func (*seqRepo) Incr(ctx context.Context, objectType int, objectId uint64) (uint64, error) {
-	tx := db.DB.Begin()
-	defer tx.Rollback()
-
 	var seq uint64
-	err := tx.WithContext(ctx).Raw("select seq from seq where object_type = ? and object_id = ? for update", objectType, objectId).
-		Row().Scan(&seq)
-	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+	err := db.DB.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		// UPSERT: 插入或更新
+		err := tx.Exec(
+			"INSERT INTO seq (object_type, object_id, seq) VALUES (?, ?, 1) "+
+				"ON DUPLICATE KEY UPDATE seq = seq + 1",
+			objectType, objectId).Error
+		if err != nil {
+			return err
+		}
+		// 在同一事务中查询当前值
+		return tx.Raw("SELECT seq FROM seq WHERE object_type = ? AND object_id = ?",
+			objectType, objectId).Row().Scan(&seq)
+	})
+	if err != nil {
 		return 0, err
 	}
-	if errors.Is(err, sql.ErrNoRows) {
-		err = tx.WithContext(ctx).Exec("insert into seq (object_type,object_id,seq) values (?,?,?)", objectType, objectId, seq+1).Error
-		if err != nil {
-			return 0, err
-		}
-	} else {
-		err = tx.WithContext(ctx).Exec("update seq set seq = seq + 1 where object_type = ? and object_id = ?", objectType, objectId).Error
-		if err != nil {
-			return 0, err
-		}
-	}
-
-	tx.Commit()
-	return seq + 1, nil
+	return seq, nil
 }
--- internal/logic/message/repo/seq_test.go
+++ internal/logic/message/repo/seq_test.go
@@ -2,10 +2,13 @@ package repo
 
 import (
 	"context"
-	"fmt"
 	"testing"
 )
 
 func Test_seqDao_Incr(t *testing.T) {
-	fmt.Println(SeqRepo.Incr(context.Background(), 1, 5))
+	seq, err := SeqRepo.Incr(context.Background(), 1, 5)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(seq)
 }
